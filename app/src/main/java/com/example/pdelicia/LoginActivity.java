8f4c6f5159b2aa73a307cff4c5c059a8285df791
ramirezf <francisco.ramirez@tecsup.edu.pe>
2021-02-04 12:10:03 -0500

    agregar permisos e implementaciones

---
+++ app/src/main/java/com/example/pdelicia/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.pdelicia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
